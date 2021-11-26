0eea8e2dd2a76c3287148feb75801815bca80d75
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-11-26 14:49:28 -0300

    Add annotation to enable dynamo repositories

--- src/main/java/com/digitalinnovation/one/heroesapi/HeroesApiApplication.java
+++ src/main/java/com/digitalinnovation/one/heroesapi/HeroesApiApplication.java
@@ -1,9 +1,11 @@
 package com.digitalinnovation.one.heroesapi;
 
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDynamoDBRepositories
 public class HeroesApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/digitalinnovation/one/heroesapi/config/HeroesData.java
+++ src/main/java/com/digitalinnovation/one/heroesapi/config/HeroesData.java
@@ -9,6 +9,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Table;
 
+
 public class HeroesData {
     public static void main (String[] args) throws Exception {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder
