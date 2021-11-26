90aa89ce815c93676e88171147459938d896b815
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-11-26 14:23:03 -0300

    Create class to insert into Heroes table

---
+++ src/main/java/com/digitalinnovation/one/heroesapi/config/HeroesData.java
@@ -0,0 +1,32 @@
+package com.digitalinnovation.one.heroesapi.config;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.digitalinnovation.one.heroesapi.constants.HeroesConstants;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+public class HeroesData {
+    public static void main (String[] args) throws Exception {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                HeroesConstants.ENDPOINT_DYNAMO,
+                                HeroesConstants.REGION_DYNAMO
+                        ))
+                .build();
+        DynamoDB dynamoDB = new DynamoDB(client);
+        Table table = dynamoDB.getTable("Heroes_Table");
+        Item hero = new Item()
+                .withPrimaryKey("id", 1)
+                .withString("name", "Batman")
+                .withString("universe", "DC")
+                .withNumber("films", 4);
+
+        PutItemOutcome outcome = table.putItem(hero);
+    }
+}
